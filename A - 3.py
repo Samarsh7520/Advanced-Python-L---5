1a69aa38319ad4a0b9944da70a0aa3ebf113b4dc
Samarsh7520 <samarsh.jaiz@gmail.com>
2026-04-19 17:36:29 +0530

    Add files via upload

---
+++ A - 3.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.title("main")
+def topwin():
+    top = Toplevel()
+    top.geometry("200x200")
+    top.title("toplevel")
+    l2 = Label(top, text="This is toplevel window")
+    l2.pack()
+    top.mainloop()
+l= Label(root, text="This is root window")
+btn = Button(root, text = "Click here", command = topwin)
+l.pack()
+btn.pack()
+root.mainloop()
